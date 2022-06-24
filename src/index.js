7abd8f0afd7cacd0e8717f6e6f6a5bfcb1bb4a37
EVGENIY-12 <93032109+EvgeniyArabadzhiyskiy@users.noreply.github.com>
2022-06-24 12:51:12 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import '/modern-normalize/modern-normalize.css';
-import '/modern-normalize/modern-normalize.css'
+// import '/modern-normalize/modern-normalize.css'
 import { App } from 'components/App';
 import './index.css';
 
