7eaebb56df36d8d3c073ff0fbb1a5f9f5f7d99fb
EVGENIY-12 <93032109+EvgeniyArabadzhiyskiy@users.noreply.github.com>
2022-07-19 16:28:48 +0300

    Update FilterContacts.jsx

--- src/components/FilterContacts/FilterContacts.jsx
+++ src/components/FilterContacts/FilterContacts.jsx
@@ -1,10 +1,10 @@
 import PropTypes from 'prop-types';
-// import Input from 'components/Input/Input';
+import Input from 'components/Input/Input';
 
 const FilterContacts = ({ value, onChangeInput }) => {
   return (
     <div>
-      
+      <Input type="text" name="search" value={value} onChange={onChangeInput} />
     </div>
   );
 };
